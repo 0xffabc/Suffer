7291093a3e4fccc628c06ec6980af721d28a92f0
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-20 19:00:05 +0500

    Create index.js

---
+++ arch/index.js
@@ -0,0 +1,48 @@
+/**
+  * Todo: separate to classes (refactor)
+  
+  * start' starts the server
+**/
+
+const net = require("net");
+
+class Client {
+  constructor(config) {
+    console.log(`[client] connecting to relay`);
+
+    this.cipher = config.cipher;
+    this.socket = net.createConnection({ host: config.host, port: config.port }, () => {
+      console.log(`[client] starting authentification process`);
+
+      socket.write(config.cipher.pack({
+        ip: config.ip
+      }));
+    });
+  }
+
+  on(a, callback) {
+    this.socket.on("data", callback);
+  }
+
+  send(protocol, data) {
+    this.socket.write(this.cipher.pack({
+      protocol, data
+    }));
+  }
+}
+
+class Server {
+  constructor(config) { }
+}
+
+module.exports = new class Architecture {
+  init(config) {
+    console.log(`[arch] loading config: ${config.host}:${config.port} ${config.isClient ? "client" : "server"}`);
+
+    if (config.isClient) {
+      Object.assign(this, new Client(config));
+    } else {
+      Object.assign(this, new Server(config));
+    }
+  }
+}
