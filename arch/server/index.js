2e89f8133033e5edeb314de1b72ee0cfe565a3e0
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-21 08:25:19 +0500

    Create index.js

---
+++ arch/server/index.js
@@ -0,0 +1,19 @@
+class Server {
+  constructor(config) {
+    this.cipher = config.cipher;
+    this.server = net.createServer(socket => {
+      socket.once("data", data => {
+        console.log(data);
+
+        console.log(`[server] piping socket to ${data}`);
+      });
+    });
+  }
+
+  start(config) {
+    this.server.listen(7912);
+  }
+}
+
+
+module.exports = Server;
