ded625649784da4b2ac15eb9ffac637bbea45bd3
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-21 08:22:38 +0500

    Update index.js

--- arch/index.js
+++ arch/index.js
@@ -32,7 +32,20 @@ class Client {
 }
 
 class Server {
-  constructor(config) { }
+  constructor(config) {
+    this.cipher = config.cipher;
+    this.server = net.createServer(socket => {
+      socket.once("data", data => {
+        console.log(data);
+
+        console.log(`[server] piping socket to ${data}`);
+      });
+    });
+  }
+
+  start(config) {
+    this.server.listen(7912);
+  }
 }
 
 module.exports = new class Architecture {
