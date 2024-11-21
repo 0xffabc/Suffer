9d9258cfc5f42c2d04c46f56d13ecd09c1a5c48f
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-21 17:24:53 +0500

    Update index.js

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const net = require('net');
+
+const server = net.createServer((clientSocket) => {
+  console.log('[server] Client connected');
+
+  clientSocket.on('data', (data) => {
+    if (data.length < 3) {
+      console.log('[server] Invalid authentication packet');
+      return clientSocket.end();
+    }
+
+    const destinationLength = data[0];
+    const destination = data.slice(1, 1 + destinationLength).toString();
+    const destPort = data.readUInt16BE(1 + destinationLength);
+
+    console.log(`[server] Authenticating to destination ${destination}:${destPort}`);
+
+    const destinationSocket = net.createConnection({ host: destination, port: destPort }, () => {
+      console.log('[server] Authentication successful!');
+
+      clientSocket.pipe(destinationSocket);
+      destinationSocket.pipe(clientSocket);
+    });
+
+    destinationSocket.on('error', (err) => {
+      console.error('[server] Destination connection error:', err);
+      clientSocket.end();
+    });
+    destinationSocket.on('end', () => {
+      console.log('[server] Destination disconnected');
+      clientSocket.end();
+    });
+    
+    clientSocket.on('error', (err) => {
+      console.error('[server] Client connection error:', err);
+      destinationSocket.end();
+    });
+    clientSocket.on('end', () => {
+      console.log('[server] Client disconnected');
+      destinationSocket.end();
+    });
+  });
+  clientSocket.on('error', (err) => {
+    console.error('[ERR!] -> ', err);
+  });
+});
+
+const PORT = global.config.port;
+server.listen(PORT, () => {
+  console.log(`[server] Listening on port ${PORT}`);
+});
