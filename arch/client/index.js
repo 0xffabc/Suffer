235b84a2cdfed8e57b065b35a16030f19d5f5ae0
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-21 08:24:45 +0500

    Create index.js

---
+++ arch/client/index.js
@@ -0,0 +1,26 @@
+class Client {
+  constructor(config) {
+    console.log(`[client] connecting to relay`);
+
+    this.cipher = config.cipher;
+    this.socket = net.createConnection({ host: config.host, port: config.port }, () => {
+      console.log(`[client] starting authentification process`);
+
+      socket.write(config.cipher.pack({
+        ip: config.ip
+      }));
+    });
+  }
+
+  on(a, callback) {
+    this.socket.on("data", callback);
+  }
+
+  send(protocol, data) {
+    this.socket.write(this.cipher.pack({
+      protocol, data
+    }));
+  }
+}
+
+module.exports = Client;
