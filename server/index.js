164dff9da834408c5db8fce74d4baf46eaf85966
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-22 20:57:11 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,21 +7,18 @@ const server = net.createServer(clientSocket => {
     if (data.length < 3) return clientSocket.end();
    
     const destinationLength = data[0];
-    const destination = data.slice(1, 1 + destinationLength).join(".");
+    const destination = data.slice(1, 1 + destinationLength).join('.');
     const destPort = data[1 + destinationLength] << 8 | data[2 + destinationLength];
     const totalLength = 3 + destinationLength;
     const splitComb = data.slice(totalLength, data.length);
 
     const destinationSocket = net.createConnection({ host: destination, port: destPort }, () => {
       console.log('[server] Authentication successful. Initializing ciphers');
-      if (splitComb.length > 0) {
-        console.log(`[antidpi-detect?] Found merged part and added to queue`, splitComb);
-        destinationSocket.write(new Uint8Array(splitComb));
-      }
+      if (splitComb.length > 0) destinationSocket.write(new Uint8Array(splitComb));
     });
 
-    clientSocket.on('data', packet => destinationSocket.write(packet));
-    destinationSocket.on('data', packet => clientSocket.write(packet));
+    clientSocket.pipe(destinationSocket);
+    destinationSocket.pipe(clientSocket);
 
     clientSocket.on('error', err => destinationSocket.end());
     destinationSocket.on('error', err => {
