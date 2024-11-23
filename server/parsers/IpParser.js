4421dd9b1967d18e74083ef258e6e179e9b0ee02
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-23 16:29:51 +0500

    Update IpParser.js

--- server/parsers/IpParser.js
+++ server/parsers/IpParser.js
@@ -1,12 +1,12 @@
 class IpParser {
   parse(data) {
-    const destinationLength = data[0];
-    const destination = data.slice(1, 1 + destinationLength).join('.');
-    const destPort = (data[1 + destinationLength] << 8) | data[2 + destinationLength];
+    const host = data.slice(1, 1 + destinationLength).join('.');
+    const port = (data[1 + destinationLength] << 8) | data[2 + destinationLength];
+    
     const totalLength = 3 + destinationLength;
     const splitComb = data.slice(totalLength, data.length);
 
-    return { destinationLength, destination, destPort, splitComb };
+    return { host, port, splitComb };
   }
 }
 
