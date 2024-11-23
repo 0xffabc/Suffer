095999673b396c3dbe3e64bfb3bf637c26e55e64
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-23 16:07:01 +0500

    Update index.js

--- client/index.js
+++ client/index.js
@@ -4,44 +4,56 @@ const net = require('net');
 
 console.log('== Suffer v1.0 -> Proxy for penetrating censorship ==');
 
-const ipParser = new IpParser();
+class Server {
+  constructor(socket) {
+    this.ipParser = new IpParser();
 
-/**
-  * Create socks5 server. We don't need authentification
-  * Because we only need to capture packets
-  * So, we omit first authentification packet and select NoAuth mode
-  * At the next packet, we automatically reply with success code
-  * And then pipe serverSocket to current socket, to cast every message on
-  * The desired server.
+    this.fakePacket = new Response("a").bytes();
+    this.authPacket = Buffer.from([5, 0]);
 
-  * Note: for each socks5 connection we create a new
-  * encrypted tunnel
-**/
+    this.messageQueue = [];
+    this.socket = socket;
 
-net.createServer(async socket => {
-  socket.once('data', data => {
-    console.log('[authlib] new client, atm skip to none auth');
-    const version = data[0];
-    const messageQueue = [];
+    this.startAuth();
+  }
 
-    if (version != 5) {
-      socket.write(await (new Response("a").bytes()));
-     
-      return socket.end();
-    } else socket.write(Buffer.from([5, 0]));
-   
+  log(data) {
+    console.log(`[${new Date().toLocaleTimeString()}] ${data}`);
+  }
+
+  onHTTPS() {
+    this.socket.write(this.fakePacket);
+    this.socket.end();
+  }
+
+  startAuth() {
     socket.once('data', data => {
-      const { host_raw, port, host, destAddrType } = ipParser.parse(data);
+      if (data[0] != 5) return this.onHTTPS();
 
-      socket.write(Buffer.from([5, 0, 0, destAddrType, ...host_raw, port >> 8, port & 0xFF]));
-      socket.on('error', () => socket.end());
-      socket.on('data', packet => {
-        if (tunnel.opened) return tunnel.send(packet);
-        messageQueue.push(packet);
-      });
-
-      const tunnel = new ClientTunnel(global.config.host, global.config.port, host_raw, port || 443, socket);
-      tunnel.onOpen = () => messageQueue.forEach(_ => tunnel.send(_));
+      socket.once('data', data => this.processURL(data));
     });
-  });
+  }
+
+  processURL(data) {
+    const { host_raw, port, destAddrType } = this.ipParser.parse(data);
+    const tunnel = new ClientTunnel(global.config.host, global.config.port, host_raw, port || 443, socket);
+  
+    tunnel.onOpen = () => this.messageQueue.forEach(tunnel.send.bind(tunnel));
+
+    this.log('Estabishing connection to ' + host_raw.join('.') + ':' + port);
+    this.accept(host_raw, port, destAddrType);
+    this.socket.on('error', () => this.socket.end());
+    this.socket.on('data', packet => {
+      if (tunnel.opened) return tunnel.send(packet);
+      this.messageQueue.push(packet);
+    });
+  }
+  
+  accept(host, port, destType) {
+    this.socket.write(Buffer.from([5, 0, 0, destType, ...host, port >> 8, port]));
+  }
+}
+
+net.createServer(socket => {
+  new Server(socket);
 }).listen(global.config.localPort || 1080);
