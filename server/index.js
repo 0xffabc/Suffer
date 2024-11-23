a40dc00e482457ddafbd17cc69b0725024a57ecf
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-23 17:52:10 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,14 @@
 const net = require('net');
 const IpParser = require('./parsers/IpParser.js');
+const Logger = require('../logger/index.js');
 
 class Server {
   constructor(socket) {
     this.socket = socket;
     this.ipParser = new IpParser();
+    this.logger = new Logger('Server');
 
     this.socket.once('data', this.start.bind(this));
-  }
-
-  log(...data) {
-    console.log(`[${new Date().toLocaleTimeString()}] ${data.join('.')}`);
   }
 
   start(data) {
@@ -19,7 +17,8 @@ class Server {
     this.destSocket = net.createConnection({ host, port }, () => {
       if (splitComb.length) this.destSocket.write(splitComb);
       
-      this.log('[server] Authentication successful.');
+      this.logger.log('Authentication successful. Piping messages');
+      this.logger.log('Split comb is ', splitComb);
     });
 
     this.socket.pipe(this.destSocket);
