a36e88cefd9c6a85e2dd9af2b25bbda8e1188f3d
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-24 15:18:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const config = require('./config/index.js');
+const Config = require('./config/index.js');
 
 console.log(`[config] Parsed from cmdline
 Tunnel endpoint -> ${config.endpoint}
 Port -> ${config.port}
 Running server? -> ${config.server}`);
 
-if (config.server) require("./server/index.js");
+if (new Config().server) require("./server/index.js");
 else require("./client/index.js");
