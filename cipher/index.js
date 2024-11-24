a7c47fc8163f146385d17948936ce1aae5484d10
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-24 18:26:38 +0500

    Update index.js

--- cipher/index.js
+++ cipher/index.js
@@ -14,14 +14,16 @@ class Cipher {
   // TODO: Add actual encryption via tls module
   async encrypt(data) {
     //const compressed = await brotliCompress(data);
+    data = Array.from(data, e => e + 1);
     this.logger.log('Sending', data);
-    return data;
+    return new Uint8Array(data);
   }
 
   async decrypt(data) {
     //const decompressed = await brotliDecompress(data);
+    data = Array.from(data, e => e + 1);
     this.logger.log('Receiving', data);
-    return data;
+    return new Uint8Array(data);
   }
 }
 
