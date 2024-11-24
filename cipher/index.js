3c2367b32f1fbd39e03dd19d5f1a21b7b8d0d5f2
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-24 18:04:20 +0500

    Update index.js

--- cipher/index.js
+++ cipher/index.js
@@ -24,41 +24,6 @@ class Cipher {
     this.logger.log(decompressed);
     return decompressed;
   }
-  
-  init(sender, receiver) {
-    /**
-      * To send a packet, sender calls "write" on receiver.
-      * Therefore, if we want to forward decrypted packet 
-      * We modify receiver's write 
-      
-      * HACK! setting '__proto__: null' makes it impossible
-      * to detect the proxy via overwriting Object.prototype.get
-      * and testing if something was caught
-    **/
-
-    receiver.write = new Proxy(receiver.write, {
-      __proto__: null,
-      apply: (async function (targetObj, thisObj, argsArr) {
-        argsArr[0] = await this.decrypt(argsArr[0]);
-        
-        return targetObj.apply(thisObj, argsArr);
-      }).bind(this)
-    });
-
-    /**
-      * The sender should send only encrypted data
-      * So we repeat the process in reverse order
-    **/
-
-    sender.write = new Proxy(sender.write, {
-      __proto__: null,
-      apply: (async function (targetObj, thisObj, argsArr) {
-        argsArr[0] = await this.encrypt(argsArr[0]);
-        
-        return targetObj.apply(thisObj, argsArr);
-      }).bind(this)
-    });
-  }
 }
 
 module.exports = Cipher;
