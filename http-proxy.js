2996246ae40ac67d8266ebad1a9a5e167967ea97
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-11-28 19:21:22 +0500

    [Upd] Simplify usage

--- http-proxy.js
+++ http-proxy.js
@@ -1,15 +1,11 @@
 const http = require('http')
 const { URL } = require('url')
 
-const server = http.createServer((req, res) => {
-  const targetUrl = new URL(req.url.slice(1), `http://${req.headers.host}`)
-  http.get(targetUrl, (response) => {
-    res.writeHead(response.statusCode, response.headers)
-    response.pipe(res)
-  }).on('error', () => {
-    res.writeHead(500)
-    res.end('Error')
-  })
+const server = http.createServer(async (req, res) => {
+  const _req = await fetch(atob(req.url.split("=")[1]));
+  const _res = await _req.arrayBuffer();
+
+  req.end(new Uint8Array(_res));
 })
 
 server.listen(3000)
